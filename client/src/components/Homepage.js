fefa72748cfbd148a1e4b8adfcac08cc4d90983c
Raaydon <69201936+Raaydon@users.noreply.github.com>
2022-01-29 12:01:34 +0000

    update

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -3,30 +3,50 @@ import { Link } from 'react-router-dom'
 
 import Button from '@mui/material/Button';
 import Input from '@mui/material/Input';
+import TextField from '@mui/material/TextField';
 
 import randomCodeGenerator from '../utils/randomCodeGenerator'
 
 const Homepage = () => {
     const [roomCode, setRoomCode] = useState('')
+    const [name, setName] = useState(null)
 
     return (
         <div className='Homepage'>
             <div className='homepage-menu'>
-                <img src={require('../assets/logo.png').default} alt='' width='200px' />
+                <div className="homepage-header">
+                    <img src={require('../assets/logo.png').default} alt='' width='200px' />
+                    <TextField
+                        label='Name'
+                        variant="filled"
+                        className="input"
+                        onChange={(e) => setName(e.target.value)}
+                        />
+                </div>
                 <div className='homepage-form'>
                     <div className='homepage-join'>
                         <Input placeholder='Game Code' onChange={(event) => setRoomCode(event.target.value)} className="input" />
                         <Button onClick={() => {
-                            if (roomCode) {
+                            if (name.length > 3) {
+                                if (roomCode) {
                                 window.href = `/play?roomCode=${roomCode}`
+                                } else {
+                                    alert('Please enter a game code')
+                                }
                             } else {
-                                alert('Please enter a game code')
+                                alert('Please enter a name longer than 3 characters')
                             }
                         }} variant="contained" className="btn">Join Game</Button>
                     </div>
                     <h1>OR</h1>
                     <div className='homepage-create'>
-                        <Link to={`/play?roomCode=${randomCodeGenerator(5)}`}><Button variant="contained" className="btn">Create Game</Button></Link>
+                        <Button variant="contained" onClick={() => {
+                            if (name.length > 3) {
+                                window.href = `/play?roomCode=${randomCodeGenerator(5)}`
+                            } else {
+                                alert('Please enter a name longer than 3 characters')
+                            }
+                        }} className="btn">Create Game</Button>
                     </div>
                 </div>
             </div>
